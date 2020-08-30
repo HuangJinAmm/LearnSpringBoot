a01048d92f008f1e1dc9f993e818a6372810ab6b
HuangJinAmm <422837038@qq.com>
2020-08-30 16:41:50 +0800

    sqrt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod n_queen;
 mod format_print;
+mod n_sqrt;
 fn main() {
     println!("hello world");
 
---
+++ src/n_sqrt.rs
@@ -0,0 +1,39 @@
+use std::ops::{Add,Div};
+fn good_enuough(guess:f64,x:f64) ->bool{
+    let diff = guess*guess - x ;
+    if diff.abs() < 0.00001f64{
+        true
+    }else{
+        false
+    }
+}
+
+fn  average(x:f64,y:f64) -> f64 {
+    (x+y)/2f64
+}
+
+fn improve(guess:f64,x:f64) ->f64 {
+    average(guess, x/guess)
+}
+
+fn sqrt_iter(guess:f64,x:f64) -> f64{
+    if good_enuough(guess, x) {
+        guess
+    } else {
+        sqrt_iter(improve(guess, x),x)
+    }
+}
+
+fn n_sqrt(x:f64) -> f64{
+    sqrt_iter(1f64, x)
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+    #[test]
+    fn test_sqrt() {
+        let x = n_sqrt(2f64);
+        println!("{}",x);
+    }
+}
