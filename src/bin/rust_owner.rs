b6cccf36604e476709c035b87b2ec294498ccdf4
hjamm <422837038@qq.com>
2022-08-18 23:09:53 +0800

    add owner

---
+++ src/bin/rust_owner.rs
@@ -0,0 +1,14 @@
+#[derive(Debug)]
+struct A{
+    a:String,
+    b:String,
+    c:String
+}
+
+fn main(){
+    let x = &A { a:"a".into(),b:"2".into(),c:"3".into()};
+    let a = x.a.clone();
+    let b = x.b.clone();
+    let c = x.c.clone();
+    println!("{},{},{},{:?}",a,b,c,x);
+}
